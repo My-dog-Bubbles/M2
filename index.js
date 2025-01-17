4f244cd23cf3af380bb0de5e05e910ed961bc2c5
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-01-17 13:50:41 -0500

    more notes, on 1/17/25

--- index.js
+++ index.js
@@ -45,24 +45,74 @@
 //// 1/14/2025
 
 // Event Listeners: //
-document.getElementById("myHeader").addEventListener("click",function(){
+/*document.getElementById("myHeader").addEventListener("click",function(){
     // function detail goes here
     document.getElementById("myHeader").style.color = "blue"
-});
+});///
 
-document.getElementById("myButton").addEventListener("click",function(){
+/*document.getElementById("myButton").addEventListener("click",function(){
     document.getElementById("myButton").style.backgroundColor = "yellow"
-});
+});*///
 
 // DIFFERECNE BETWEEN .innerHTML and .textContent: //
-console.log(document.getElementById("Para2".innerHTML));
+///console.log(document.getElementById("Para2".innerHTML));
 // sees text a html format and it is not for user input
 
-console.log(document.getElementById("Para2".textContent));
+///console.log(document.getElementById("Para2".textContent));
 // Used for user imput, sees text as text only, best to use for use inputs because if a user was to put html code innerHTML then it would mess with the code.
 
+// Variables //
+///let x;
+    // has to be declared
+    //var is another old seporite version
+///console.log(x);
+///x=2;
+///console.log(x);
+///const y=10;
+// has to be initialized
 
-    
 
-    
+//BOB
+//let s_name = "angel";
+/*let s_name = prompt("wHAT IS YOUR NAME: ")
+console.log(s_name);//name is reserved
+window.alert(`HELLO, my name is $(s_name)`); // have to use backtic*/
+
+let online = false;
+console.log(online)
+console.log(`Online = ${online}!`)//use curly bracket
+
+//Arithmeti operators//
+// +, *, -, /, **, %, ++, --
+
+let student = 24;
+console.log(student)
+student = student + 1;
+console.log(student);
+student += 1;
+console.log(student);// youo can do this with any operator
+
+console.log(typeof student);
+console.log(student, typeof student);
+
+myNumber = 5;
+console.log(myNumber, typeof myNumber);
+myNumber = 5.5;
+console.log(myNumber, typeof myNumber);
+
+let myAge = prompt("WHATS YOUR AGE");
+console.log(myAge, typeof myAge);
+myAge = Number(myAge);
+console.log(myAge, typeof myAge);
+
+let x ='pizza'
+let y = "pizza"
+let z = "pizza"
+x = Number(x)
+console.log(x) 
+
+//can convert into string
+
+z = Boolean(z) // string of false equals true but empty string equals false
+console.log(z, typeof z)
 
