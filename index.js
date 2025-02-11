c3b03aff03a122fde3664cdb64d7491b1a53d9fa
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-02-11 13:59:46 -0500

    notes for 2/11/25

--- index.js
+++ index.js
@@ -78,41 +78,91 @@
 console.log(s_name);//name is reserved
 window.alert(`HELLO, my name is $(s_name)`); // have to use backtic*/
 
-let online = false;
-console.log(online)
-console.log(`Online = ${online}!`)//use curly bracket
+// let online = false;
+// console.log(online)
+// console.log(`Online = ${online}!`)//use curly bracket
 
-//Arithmeti operators//
-// +, *, -, /, **, %, ++, --
+// //Arithmeti operators//
+// // +, *, -, /, **, %, ++, --
 
-let student = 24;
-console.log(student)
-student = student + 1;
-console.log(student);
-student += 1;
-console.log(student);// youo can do this with any operator
+// let student = 24;
+// console.log(student)
+// student = student + 1;
+// console.log(student);
+// student += 1;
+// console.log(student);// you can do this with any operator
 
-console.log(typeof student);
-console.log(student, typeof student);
+// console.log(typeof student);
+// console.log(student, typeof student);
 
-myNumber = 5;
-console.log(myNumber, typeof myNumber);
-myNumber = 5.5;
-console.log(myNumber, typeof myNumber);
+// myNumber = 5;
+// console.log(myNumber, typeof myNumber);
+// myNumber = 5.5;
+// console.log(myNumber, typeof myNumber);
 
-let myAge = prompt("WHATS YOUR AGE");
-console.log(myAge, typeof myAge);
-myAge = Number(myAge);
-console.log(myAge, typeof myAge);
+// let myAge = prompt("WHATS YOUR AGE");
+// console.log(myAge, typeof myAge);
+// myAge = Number(myAge);
+// console.log(myAge, typeof myAge);
 
-let x ='pizza'
-let y = "pizza"
-let z = "pizza"
-x = Number(x)
-console.log(x) 
+// let x ='pizza'
+// let y = "pizza"
+// let z = "pizza"
+// x = Number(x)
+// console.log(x) 
 
-//can convert into string
+// //can convert into string
 
-z = Boolean(z) // string of false equals true but empty string equals false
-console.log(z, typeof z)
+// z = Boolean(z) // string of false equals true but empty string equals false
+// console.log(z, typeof z)
 
+/////2/11/25
+// let x = 1.99
+// let y = 4
+// let z = 8.2
+
+// console.log(Math.PI)
+// console.log(Math.min(x,y,z))
+// console.log(Math.max(x,y,z))
+// console.log(Math.sqrt(y))
+// console.log(Math.round(x))
+// console.log(Math.floor(x))
+// console.log(Math.ceil(x))
+// console.log(Math.trunc(z)) // remove decimal
+// console.log(Math.pow(x,y))
+// console.log(Math.sign(5))
+// console.log(Math.random)
+// console.log(Math.random()*10)
+
+// let min = 1
+// let max = 100
+
+// function getRbdIntExclusive(min,max){
+//     return Math.floor(Math.random() * (max-min))+min
+// }
+
+// function getRbdIntInclusive(min,max){
+//     return Math.floor(Math.random() * (max-min +1))-min
+// }
+
+// spread operator=iterate items
+let numbers = [1,2,3,4,5]
+let errormax = Math.max(numbers)
+let newmax = Math.max(...numbers)// the three dots are the spread operator
+console.log(errormax)
+console.log(newmax)
+
+let username = "angel .C"
+let letters = [...username]
+console.log(username)
+console.log(letters)
+
+let assembleuser = [...username].join("-")
+console.log(assembleuser)
+foods = [...fruits, ...vegi]
+console.log(foods)
+
+let fruits = ["apple","banana","peach"]
+let vegi = ["celery","carrot","brocoli"]
+foods = [...fruits, ...vegi, "eggs", "milk"]
+console.log(foods)
