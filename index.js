e2fc80833aff1b0bd62441c2657a5675248afe9a
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-02-12 13:46:26 -0500

    2/12/25 notes

--- index.js
+++ index.js
@@ -145,24 +145,124 @@ window.alert(`HELLO, my name is $(s_name)`); // have to use backtic*/
 //     return Math.floor(Math.random() * (max-min +1))-min
 // }
 
-// spread operator=iterate items
-let numbers = [1,2,3,4,5]
-let errormax = Math.max(numbers)
-let newmax = Math.max(...numbers)// the three dots are the spread operator
-console.log(errormax)
-console.log(newmax)
+/// spread operator=iterate items
+// let numbers = [1,2,3,4,5]
+// let errormax = Math.max(numbers)
+// let newmax = Math.max(...numbers)// the three dots are the spread operator
+// console.log(errormax)
+// console.log(newmax)
 
-let username = "angel .C"
-let letters = [...username]
-console.log(username)
-console.log(letters)
+// let username = "angel .C"
+// let letters = [...username]
+// console.log(username)
+// console.log(letters)
 
-let assembleuser = [...username].join("-")
-console.log(assembleuser)
-foods = [...fruits, ...vegi]
-console.log(foods)
+// let assembleuser = [...username].join("-")
+// console.log(assembleuser)
+// foods = [...fruits, ...vegi]
+// console.log(foods)
 
-let fruits = ["apple","banana","peach"]
-let vegi = ["celery","carrot","brocoli"]
-foods = [...fruits, ...vegi, "eggs", "milk"]
-console.log(foods)
+// let fruits = ["apple","banana","peach"]
+// let vegi = ["celery","carrot","brocoli"]
+// foods = [...fruits, ...vegi, "eggs", "milk"]
+// console.log(foods)
+
+let x = true
+let y = false
+let z = true
+
+// simple boolean check
+
+if(x){
+    console.log("this is true")
+} else{
+    console.log("this is false")
+}
+
+// and &&
+if (x&& y){
+    console.log("x and y are true")
+} else{
+    console.log("they are not both true")
+}
+
+// or ||
+if(x||y){
+    console.log("x or y is true")
+} else{
+    console.log("neither x nor y is true")
+}
+
+// not !
+if (!x){
+    console.log("x is not true") // check for the oppsite of the value of x
+} else{
+    console.log("x is true")
+}
+
+
+const temp = 35
+if(temp>0){
+    console.log("the weather is above 0")
+}else if(temp == 32){
+    console.log("temp has just hit freezing")
+} else{
+    console.log("the temp is below 0")
+}
+
+// check range with logical opperatrs
+
+if (temp >70 && temp <80){
+    console.log("temp is ideal for the pool")
+} else {
+    console.log("temp is not idel")
+}
+
+// check with or ||
+if (temp >90 || temp <60){
+    console.log("temp is extrme hot or cold")
+} else {
+    console.log("temp is with a excitable range")
+}
+
+// 
+if (!(temp >100 || temp <50)){
+    console.log("temp is with a excitable range")
+} else {
+    console.log("temp is extrme hot or cold")
+}
+
+//Equality & strict equality
+/*
+= assigning
+== comparison/ check if values are equal
+=== strict equality/ compares if values and data type are equal
+!= inequality
+!== strict inequality
+*/
+const pi = 3.14
+console.log(typeof pi)
+if (pi == "3.14"){//will say true
+    console.log("that is pie")
+} else{
+    console.log("that is not pie")
+}
+
+if (pi === "3.14"){///will say false
+    console.log("that is pie")
+} else{
+    console.log("that is not pie")
+}
+
+if (pi != "3.14"){///will say false
+    console.log("that is not pie")
+} else{
+    console.log("that is pie")
+}
+
+// they are strictly not equalry which evauat e to two, triggering the if statement
+if (pi !== "3.14"){///will say true
+    console.log("that is not pie")
+} else{
+    console.log("that is pie")
+}
